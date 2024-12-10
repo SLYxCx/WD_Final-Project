7a378f2c180111a97940b2d833bb8f44df78ec93
pedro40002 <147000860+pedro40002@users.noreply.github.com>
2024-12-10 16:14:31 -0700

    Update page.js

--- adven_chore/app/page.js
+++ adven_chore/app/page.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [health, setHealth] = useState(75); // Set initial health to 75%
+  const [health, setHealth] = useState(75);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
