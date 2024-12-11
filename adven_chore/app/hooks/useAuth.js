e72192befedfcba345e362e0fd3b656693b89f74
Steven <scole.notredame23@gmail.com>
2024-12-10 19:26:47 -0700

    Working on Firebase and NextAuth
    setting up Firebase and Next Auth

---
+++ adven_chore/app/hooks/useAuth.js
@@ -0,0 +1,29 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase/config";
+
+const useAuth = () => {
+    const [user, loading] = useAuthState(auth);
+    const [session, setSession] = useState(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        if(typeof window !== "undefined") {
+            const storedSession = sessionStorage.getItem("user");
+            setSession(storedSession);
+        }
+    }, []);
+
+    useEffect(() => {
+        if(!loading && !user && !session) {
+            router.push('/');
+        }
+    }, [user, loading, session, router]);
+
+    return { user, loading };
+};
+
+export default useAuth;
