205f69bf5a59e1ce1ae40a94a18bde5cdf604d77
Steven <scole.notredame23@gmail.com>
2024-12-14 08:18:18 -0700

    Google

--- adven_chore/app/components/SignIn.jsx
+++ adven_chore/app/components/SignIn.jsx
@@ -1,21 +1,23 @@
 'use client'
 import React from "react";
 import { useRouter } from "next/navigation";
-import { useSignInWithEmailLink } from "react-firebase-hooks/auth";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { auth } from '../firebase/config';
 import { useState } from 'react';
 
 const SignIn = () => {
     const [email, setEmail] = useState("")
-    const SignInWithEmailLink = useSignInWithEmailLink(auth)
+    const [password, setPassword] = useState("")
+    const SignInWithEmailAndPassword = useSignInWithEmailAndPassword(auth)
     const router = useRouter()
 
     const handleSignIn = async () => {
 
         try {
-            const res = await SignInWithEmailLink(email)
+            const res = await SignInWithEmailAndPassword(email, password)
             sessionStorage.setItem('user', 'true')
             setEmail('')
+            setPassword('')
             router.push('/')
 
         } catch (error) {
@@ -25,12 +27,26 @@ const SignIn = () => {
 
     return (
         <div>
-            <label>Email</label>
             <input
                 type="email"
+                placeholder="Email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
             />
+            <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-3 mb-4 bg-gray-700 rounded outline-non text-white placeholder-gray-500"
+            />
+            <button
+            onClick={handleSignIn}
+            className="w-full p-3 bg-blue-500 text-white rounded mt-4 hover:bg-blue-600"
+            >
+            Sign In
+            </button>
         </div>
     );
 };
